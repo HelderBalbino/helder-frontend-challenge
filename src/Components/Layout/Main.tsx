6785848c16185015e094f14cb8ceb8860cc9d317
Helder Balbino <helderbalbino@googlemail.com>
2025-05-30 20:44:14 +0100

    Refactor Main component to use a named export for better readability and maintainability

--- src/Components/Layout/Main.tsx
+++ src/Components/Layout/Main.tsx
@@ -1,12 +1,15 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export default styled.section`
-  display: block;
-  width: 100%;
-  max-width: 980px;
-  margin: 0 auto;
+const Container = styled.section`
+	display: block;
+	width: 100%;
+	max-width: 980px;
+	margin: 0 auto;
+	padding: 0 32px;
 
-  @media (max-width: ${(props) => props.theme.breakpoints.small}) {
-    padding: 150px;
-  }
+	@media (max-width: ${(props) => props.theme.breakpoints.small}) {
+		padding: 24px 16px;
+	}
 `;
+
+export default Container;
