b8364212ad1affb28734f309424111e3f2e3b025
Helder Balbino <helderbalbino@googlemail.com>
2025-06-03 14:44:46 +0100

    Remove logo image references from Header component

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 const Header = ({ className }: HeaderProps) => {
 	return (
 		<div className={className}>
-			<img src='Logo.svg' alt='Huler Todo' />
+			<img src='' alt='' />
 		</div>
 	);
 };
